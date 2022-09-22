f15c7c2a048598478f54f226703827d9efaaf423
Jelle Groothuysen <74619141+jellegro@users.noreply.github.com>
2022-09-22 12:02:22 +0200

    namespace update - IncidentService

--- gb_api/Incident/Application/IncidentService.cs
+++ gb_api/Incident/Application/IncidentService.cs
@@ -1,30 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using gb_api.Incident.Domain;
-
-namespace IncidentPackage.Application
+namespace gb_api.Incident.Application
 {
     public static class IncidentService
     {
-        static List<Incident> Incidents { get; }
+        static List<Domain.Incident> Incidents { get; }
         static IncidentService()
         {
-            Incidents = new List<Incident>
+            Incidents = new List<Domain.Incident>
             {
-                new Incident(2L),
-                new Incident(3L)
+                new Domain.Incident(2L),
+                new Domain.Incident(3L)
             };
         }
 
-        public static void Add(Incident incident)
+        public static void Add(Domain.Incident incident)
         {
             Incidents.Add(incident);  
         }
 
-        public static Incident? Get(long id)
+        public static Domain.Incident? Get(long id)
         {
             return Incidents.FirstOrDefault(ic => ic.Id == id);
         }
