b9fa758d779e28ad31ba074e35f18d5a3ab3d274
tvanoorschot <oorschot98@gmail.com>
2022-09-23 20:59:16 +0200

    extra test

--- gb_apiTests/Incident/Domain/IncidentTests.cs
+++ gb_apiTests/Incident/Domain/IncidentTests.cs
@@ -1,14 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using IncidentPackage.Domain;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using gb_api.Incident.Domain;
 
-namespace IncidentPackage.Domain.Tests
+namespace gb_apiTests.Incident.Domain
 {
     [TestClass()]
     public class IncidentTests
@@ -16,7 +9,7 @@ namespace IncidentPackage.Domain.Tests
         [TestMethod()]
         public void IncidentTest()
         {
-            var incident = new Incident(21);
+            var incident = new gb_api.Incident.Domain.Incident(21, "test");
             Debug.Assert(incident.Id == 21);
         }
     }
